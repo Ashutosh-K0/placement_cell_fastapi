fdbc4980d4c8bf41b0272df983c317303797b9c4
Ashutosh-K0 <ashutosh.kumar2022b@vitstudent.ac.in>
2026-06-27 03:17:09 +0530

    Student Profile SQLAlchemy Model

---
+++ app/models/student_profile.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, Float, DateTime, func, ForeignKey
+from sqlalchemy.orm import relationship
+from app.database.database import Base
+
+class StudentProfile(Base):
+    __tablename__ = 'student_profiles'
+    user_id = Column(Integer,ForeignKey("users.id"),primary_key=True)
+    college_name = Column(String(200), nullable=False)
+    branch = Column(String(150), nullable=False)
+    cgpa = Column(Float, nullable=False)
+    tenth_percentage = Column(Float, nullable=False)
+    twelfth_percentage = Column(Float, nullable=False)
+    backlog_count = Column(Integer, nullable=False, default=0)
+    address = Column(String(500), nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate= func.now(), nullable=False)
