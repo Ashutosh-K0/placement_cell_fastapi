39666c2f433d74ba84a6b4c3a950f31bd77a70df
Ashutosh-K0 <ashutosh.kumar2022b@vitstudent.ac.in>
2026-06-27 07:10:50 +0530

    Application History SqlAlchemy Model Created

---
+++ app/models/application_history.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, func, DateTime, Enum, Text, ForeignKey
+from sqlalchemy.orm import relationship
+from app.database.database import Base
+from app.enums.application_status import ApplicationStatus
+
+class ApplicationHistory(Base):
+    __tablename__ = 'application_history'
+    id = Column(Integer, primary_key=True)
+    application_id = Column(Integer, ForeignKey("applications.id"), nullable=False)
+    changed_by_user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    old_status = Column(Enum(ApplicationStatus), nullable=True)
+    new_status = Column(Enum(ApplicationStatus), nullable=False)
+    notes = Column(Text, nullable=True)
+    created_at = Column(DateTime(timezone=True) server_default=func.now(), nullable=False)
