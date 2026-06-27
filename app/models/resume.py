870d2fb112a40c79a1875af8b37be31e8527f97d
Ashutosh-K0 <ashutosh.kumar2022b@vitstudent.ac.in>
2026-06-27 06:35:52 +0530

    Resume SQLAlchemy Model Created

---
+++ app/models/resume.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, Boolean, ForeignKey, String, func, DateTime
+from sqlalchemy.orm import relationship
+from app.database.database import Base
+
+class Resume(Base):
+    __tablename__ = 'resumes'
+    id = Column(Integer, primary_key=True)
+    student_id = Column(Integer, ForeignKey("users.id"), nullable=False, index=True)
+    title = Column(String(100), nullable=False, unique=True)
+    file_url = Column(String(255), nullable=False)
+    is_primary = Column(Boolean, default=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
